c41d48c6fad0c88659506091977a541638a0b17f
Faiz Surani <faiz.surani@gmail.com>
2020-05-03 15:47:21 -0700

    More patient form work

---
+++ src/components/Patients/PatientForm/FieldValueGroupSection.jsx
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from 'react';
+import PropTypes from 'prop-types';
+
+class FieldValueGroupSection extends Component {
+    render() {
+        return (
+            <Fragment>
+
+            </Fragment>
+        );
+    }
+}
+
+FieldValueGroupSection.propTypes = {
+    fieldGroup: PropTypes.object.isRequired,
+    fieldValueGroup:
+};
+
+export default FieldValueGroupSection;
