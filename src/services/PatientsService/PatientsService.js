e32869c7b1f89261fe55a09025e481d2ce4c076c
Faiz Surani <faiz.surani@gmail.com>
2020-05-03 13:29:35 -0700

    Fixed issue with patient routing

--- src/services/PatientsService/PatientsService.js
+++ src/services/PatientsService/PatientsService.js
@@ -1,17 +1,14 @@
 import axios from "axios";
-import {patientApiUrl} from "../service-constants";
+import {patientApiUrl, patientsApiUrl} from "../service-constants";
 
 class PatientsService {
-    static patientUrl(id) {
-        return `${patientApiUrl}/${id}`;
-    }
 
     static async getPatients() {
-        return axios.get(patientApiUrl).then(response => response.data);
+        return axios.get(patientsApiUrl).then(response => response.data);
     }
 
     static async getPatient(id) {
-        return axios.get(this.patientUrl(id)).then(response => response.data);
+        return axios.get(patientApiUrl(id)).then(response => response.data);
     }
 
     static async updatePatient(id, patient) {
