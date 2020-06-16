dae54425ed0bfeb90edc9b059f87fb726ec1ab10
Faiz Surani <faiz.surani@gmail.com>
2020-06-16 00:05:25 -0700

    Edit components implemented, no data update yet

--- src/components/Fields/FieldsTable.jsx
+++ src/components/Fields/FieldsTable.jsx
@@ -5,6 +5,10 @@ import {tableIcons} from "../common/TableHelpers";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
+import {MuiPickersUtilsProvider} from "@material-ui/pickers";
+import {DatePicker} from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+import Tooltip from "@material-ui/core/Tooltip";
 
 class FieldsTable extends Component {
     renderFieldTypeEdit(t) {
@@ -17,6 +21,11 @@ class FieldsTable extends Component {
                         console.group(e.target.value);
                         this.currentValueType = e.target.value;
                     }}
+                    InputProps={{
+                        style: {
+                            fontSize: 14
+                        }
+                    }}
                 >
                     <MenuItem value={'Text'}>{'Text'}</MenuItem>
                     <MenuItem value={'Paragraph'}>{'Paragraph'}</MenuItem>
@@ -28,6 +37,42 @@ class FieldsTable extends Component {
 
     renderDefaultValueEdit(t) {
         const editType = t.rowData.type ?? this.currentValueType;
+        if (editType === 'Paragraph') {
+            return (
+                <TextField
+                    id="outlined-multiline-static"
+                    style={{width: "300px"}} // This is kind of hacky but I can't figure out auto-resizing of the field.
+                    multiline
+                    rows={3}
+                    value={t.value}
+                    onChange={e => t.onChange(e.target.value)}
+                    variant="outlined"
+                    InputProps={{
+                        style: {
+                            fontSize: 14
+                        }
+                    }}
+                />
+            );
+        }
+        else if (editType === 'Date') {
+            return (
+                <MuiPickersUtilsProvider utils={MomentUtils}>
+                    <DatePicker
+                        format='MM/DD/yyyy'
+                        value={t.value || null}
+                        placeholder={t.columnDef.title}
+                        onChange={t.onChange}
+                        clearable
+                        InputProps={{
+                            style: {
+                                fontSize: 14
+                            }
+                        }}
+                    />
+                </MuiPickersUtilsProvider>
+            );
+        }
         return (
             <Fragment>
                 <TextField
@@ -36,24 +81,12 @@ class FieldsTable extends Component {
                     placeholder={t.columnDef.title}
                     InputProps={{
                         style: {
-                            fontSize: 13,
+                            fontSize: 14
                         }
                     }}
                 />
             </Fragment>
         );
-    }
-
-    renderTextEdit(t) {
-
-    }
-
-    renderParagraphEdit(t) {
-
-    }
-
-    renderDateEdit(t) {
-
     }
 
     render() {
@@ -71,8 +104,7 @@ class FieldsTable extends Component {
                     data={this.props.fields}
                     title="Fields"
                     options={{
-                        actionsColumnIndex: -1,
-                        // search: false
+                        actionsColumnIndex: -1
                     }}
                     editable={{
                         onRowAdd: newData =>
