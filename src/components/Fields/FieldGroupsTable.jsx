1ed9756463d4b4e8ae90adb09856edcdb67da5c7
Faiz Surani <faiz.surani@gmail.com>
2020-06-16 12:24:17 -0700

    Readded confirmation dialog and tweaked field value component

--- src/components/Fields/FieldGroupsTable.jsx
+++ src/components/Fields/FieldGroupsTable.jsx
@@ -29,7 +29,7 @@ class FieldGroupsTable extends Component {
             const groupToUpdateIndex = this.state.fieldGroups.findIndex(fg => fg.id === oldGroup.id);
             if (groupToUpdateIndex >= 0) {
                 const newFieldGroupsState = update(this.state.fieldGroups, {
-                    $splice: [[groupToUpdateIndex, 1, fieldGroupResponse]]
+                    $splice: [[groupToUpdateIndex, 1, newGroup]]
                 });
                 this.setState({fieldGroups: newFieldGroupsState});
             }
@@ -76,7 +76,8 @@ class FieldGroupsTable extends Component {
                     icons={tableIcons}
                     columns={[
                         {title: 'Name', field: 'name'},
-                        {title: 'Description', field: 'description'}
+                        {title: 'Description', field: 'description'},
+                        {title: 'Is Default', field: 'isDefaultGroup', type: 'boolean'}
                     ]}
                     data={this.state.fieldGroups}
                     title=""
