11fbce4c8f3d753da19fbf052381b3941528a212
Faiz Surani <faiz.surani@gmail.com>
2020-07-04 12:21:45 -0700

    Copyright link opens in browser, fixes #36

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
+// This is not strictly necessary for the Electron app to work, but is used so the app can still be loaded/debugged in a browser.
+const electron = window.require ? window.require('electron') : null;
 
 class Footer extends Component {
     render() {
@@ -8,7 +10,8 @@ class Footer extends Component {
             <footer className={this.props.className}>
                 <Typography variant="body2" color="textSecondary" align="center">
                     {'Copyright © '}
-                    <Link color="inherit" href="https://github.com/DrDocx">
+                    <Link color="inherit"
+                          onClick={() => electron && electron.remote.shell.openExternal('https://github.com/DrDocx')}>
                         DrDocx
                     </Link>{' '}
                     {new Date().getFullYear()}
