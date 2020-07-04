0111bfc1c4906c4d839b65a8ce0bb9a57b81d7ee
Faiz Surani <faiz.surani@gmail.com>
2020-07-04 12:38:14 -0700

    Fix minor ui issues on show patient page

--- src/components/Patients/ShowPatient.jsx
+++ src/components/Patients/ShowPatient.jsx
@@ -1,51 +1,47 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import * as PropTypes from 'prop-types';
-import { Link, withRouter } from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import MainContainer from "../common/MainContainer";
 import CreateReport from "./CreateReport";
-import { extend } from 'immutability-helper';
+import {extend} from 'immutability-helper';
 
 import StaticFieldValueGroupSection from "./PatientForm/StaticFieldValueGroupForm";
-import { TextField, Button } from "@material-ui/core";
+import {TextField, Button} from "@material-ui/core";
 import Tooltip from "@material-ui/core/Tooltip";
 import Grid from "@material-ui/core/Grid";
 
 class ShowPatient extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = { patient: this.props.patient };
-	}
+    constructor(props) {
+        super(props);
+        this.state = {patient: this.props.patient};
+    }
 
-	render() {
-		const p = this.props.patient;
-		return (
-			<Fragment>
-				<div class='testClass'>
-					Testing class styling
-				</div>
-				<MainContainer title={p.name}>
-					<Grid container alignItems="flex-start" alignContent={"flex-start"} style={{ width: "600px" }}>
-						<TextField disabled value={this.state.patient.name}
-							label="Name" />
-					</Grid>
-					{this.state.patient.fieldValueGroups.map(fvg =>
-						<StaticFieldValueGroupSection key={fvg.fieldGroupId} fieldValueGroup={fvg} />
-					)}
-					<Link to={`${this.props.match.url}/edit`}>
-						<Button variant="contained" color="primary">
-							Edit Patient
-						</Button>
-					</Link>
-					<CreateReport patient={p} />
-				</MainContainer>
-			</Fragment>
-		);
-	}
+    render() {
+        const p = this.props.patient;
+        return (
+            <Fragment>
+                <MainContainer title={p.name}>
+                    <Grid container alignItems="flex-start" alignContent={"flex-start"} style={{width: "600px"}}>
+                        <TextField disabled value={this.state.patient.name}
+                                   label="Name"/>
+                    </Grid>
+                    {this.state.patient.fieldValueGroups.map(fvg =>
+                        <StaticFieldValueGroupSection key={fvg.fieldGroupId} fieldValueGroup={fvg}/>
+                    )}
+                    <CreateReport patient={p}/>
+                    <Button variant="contained" color="primary"
+                            onClick={() => this.props.history.push(`${this.props.match.url}/edit`)}>
+                        Edit Patient
+                    </Button>
+                </MainContainer>
+            </Fragment>
+        );
+    }
 }
 
 ShowPatient.propTypes = {
-	patient: PropTypes.object.isRequired
+    patient: PropTypes.object.isRequired
 };
 
 export default withRouter(ShowPatient);
